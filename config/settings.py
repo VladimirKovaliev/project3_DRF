69d72abccafcf8775fa04ebf90d96143a76fcf20
VladimirKovaliev <kovaliev.v@list.ru>
2024-02-27 17:55:24 +0300

    урок 26.1

--- config/settings.py
+++ config/settings.py
@@ -134,9 +134,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.AllowAny',
-    ]
+    ),
 }
 
 CORS_ALLOWED_ORIGINS = [
